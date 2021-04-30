0b346336db6d502c79e4f8350c09ee3ffb52f43c
Érica Rodrigues <71781669+Erica34124@users.noreply.github.com>
2021-04-30 11:31:14 -0300

    alterações
    andamento do website

---
+++ main.js
@@ -0,0 +1,25 @@
+
+var nome = "Érica Vanessa"
+var idade= 39;
+var n1= 10;
+var n2= 20;
+var frase= "eu amo voley"
+var lista=["morango","maçã", "pera"]
+var frutas = [{nome:"manga" ,quantidade:"10"},{nome: "caju", quantidade:"20"}]
+alert("Alo mundo Java Script! " + nome + " tem "+ idade+ " anos "); 
+console.log(nome);
+console.log(idade)
+console.log(n1+n2)
+console.log(frase)
+console.log(frase.replace("eu" , "voce"));
+console.log(frase.toUpperCase())
+console.log(frase.replace("eu" , "Maria"))
+console.log(lista[2])
+console.log(lista)
+lista.pop("maçã")
+lista.push("banana")
+console.log(lista)
+lista.pop(lista[0])
+console.log(lista.length)
+console.log(lista)
+console.log(frutas.nome)
